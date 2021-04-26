2ed51b4367cdd428caaced55dce32d5bbdf18a47
MrNewaz <saifur.saif@northsouth.edu>
2021-04-26 14:22:15 +0600

    2nd update

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const app = express();
 const rootCall = (req, res) => res.send('Thank You Very Much');
 
 app.get('/', (req, res) => {
-  res.send('Thank you very much----Yeeeet');
+  const fruit = {
+    products: 'ada',
+    price: 220,
+  };
+  res.send(fruit);
+});
+
+app.get('/fruits/banana', (req, res) => {
+  res.send({ fruit: 'banana', quantity: 1000, price: 100000 });
 });
 
 app.listen(4200, () => console.log('listening to port 4200'));
